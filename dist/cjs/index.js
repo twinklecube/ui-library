efb98fc20b2f56560f3d8519779024c73b6e464d
TwinkleCube <twinklecubeui@gmail.com>
2023-06-13 12:24:31 +0200

    fixes a small error

---
+++ dist/cjs/index.js
@@ -0,0 +1,1 @@
+"use strict";var e=require("react");exports.Footer=function(){return e.createElement("div",null,"footer - ui")};
---
+++ dist/esm/index.js
@@ -0,0 +1,1 @@
+import r from"react";var e=function(){return r.createElement("div",null,"footer - ui")};export{e as Footer};
